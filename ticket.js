cd6aac271283a7e1e6cdc9c4d94f22d093e97ae2
masud <masudranaacb@gmail.com>
2024-02-18 22:10:30 +0600

    added condition only 4seat select

--- ticket.js
+++ ticket.js
@@ -1,11 +1,23 @@
 
 const priemBtns = document.getElementsByClassName("priem")
 let count = 0
+let count2 = 0
 for (let btn of priemBtns) {
     btn.addEventListener("click", function (event) {
         count += 1
+        count2 += 550
+        // start only 4 element selected
+        const addedTitalItem = document.getElementsByClassName("total_added_items")
+
+        if (addedTitalItem.length === 4) {
+            alert("plase only for your 4 seats")
+            return
+        }
+        // start only 4 element selected
         let targetSeatText = event.target.innerText
-        console.log(event.target.innerText)
+        event.target.classList.add("bg-green-700")
+        event.target.setAttribute("disabled", "text-red-700")
+        console.log(event.target)
 
         const totalSeatText = document.getElementById("total_seat").innerText
         const totalSeatNumber = parseInt(totalSeatText)
@@ -14,7 +26,7 @@ for (let btn of priemBtns) {
         const selectedItems = document.getElementById("select_items")
 
         const div = document.createElement("div")
-        div.classList.add("flex", "justify-between")
+        div.classList.add("flex", "justify-between", "total_added_items")
 
         const p1 = document.createElement("p")
         p1.innerText = targetSeatText
@@ -37,6 +49,8 @@ for (let btn of priemBtns) {
 
 
 
+        setInnerText("grand_total_price", count2)
+        setInnerText("total_price", count2)
         setInnerText("select_count", count)
         setInnerText("total_seat", totalSeatNumber - 1)
 
