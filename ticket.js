668cc242f529d2acc76f89e4683e1357033fc6fc
masud <masudranaacb@gmail.com>
2024-02-18 21:33:38 +0600

    added seat card

--- ticket.js
+++ ticket.js
@@ -4,13 +4,42 @@ let count = 0
 for (let btn of priemBtns) {
     btn.addEventListener("click", function (event) {
         count += 1
-        const totalSeatText = document.getElementById("total_seat").innerText
-        const totalSeatNumber=parseInt(totalSeatText)
+        let targetSeatText = event.target.innerText
+        console.log(event.target.innerText)
 
-        
+        const totalSeatText = document.getElementById("total_seat").innerText
+        const totalSeatNumber = parseInt(totalSeatText)
+
+        // start Selected seat added
+        const selectedItems = document.getElementById("select_items")
+
+        const div = document.createElement("div")
+        div.classList.add("flex", "justify-between")
+
+        const p1 = document.createElement("p")
+        p1.innerText = targetSeatText
+        div.appendChild(p1)
+
+        const p2 = document.createElement("p")
+        p2.innerText = "Economoy"
+        div.appendChild(p2)
+
+        const p3 = document.createElement("p")
+        p3.innerText = 550
+        div.appendChild(p3)
+
+        selectedItems.appendChild(div)
+        //end Selected seat added
+
+
+
+
+
+
+
         setInnerText("select_count", count)
-        setInnerText("total_seat",totalSeatNumber-1)
-    
+        setInnerText("total_seat", totalSeatNumber - 1)
+
     })
 }
 
